73eda5c14785221a7d47154815708cf0b38f03dd
조석준 <vcxttlvcx@naver.com>
2021-03-16 17:26:10 +0900

    update S04P22D102-45 : Member Router 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ import datetime
 from database.db import session
 from models.model import Member, MemberTable
 from crud import member_crud as crud
+from router import member_router
 
 app = FastAPI()
 
@@ -18,47 +19,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
-@app.get("/members")
-async def read_members():
-    members = crud.get_members(session)
-    return members
-
-
-@app.get("/members/{member_uuid}")
-async def read_member(member_uuid: int):
-    member = crud.get_member_by_uuid(session, member_uuid)
-    return member
-
-
-@app.post("/members")
-async def create_members(member: Member):
-    # 중복체크인 나중에 따로 db에 데이터 추가해서 걸러야할듯
-    db_member = crud.get_member_by_name(session, member.name)
-    if db_member:
-         raise HTTPException(status_code=400, detail="?? already registered")
-    
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    db_member = crud.create_member(session, member, now)
-    if db_member is None:
-        raise HTTPException(status_code=400, detail="Creation failure")
-
-    return db_member
-
-@app.put("/members")
-async def update_members(member: Member):
-    db_member = crud.get_member_by_name(session, member.name)
-    if db_member is None:
-        raise HTTPException(status_code=400, detail="No members")
-    db_member = crud.update_member(session, db_member, member)
-    print(db_member)
-    return db_member
-
-@app.delete("/members")
-async def delete_members(member_uuid: int):
-    db_member = crud.delete_member_by_uuid(session,member_uuid)
-    if db_member is None:
-        raise HTTPException(status_code=400, detail="Delete failure")
-    return db_member
-    
+app.include_router(member_router.router, tags=["Member"], prefix="/member")
