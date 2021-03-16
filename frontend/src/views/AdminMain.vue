a4bd0547748061caba9b2832a4f4aab182074a74
조준형 <cjh951114@naver.com>
2021-03-16 17:45:29 +0900

    update S04P22D102-44 : 각 페이지 틀제작, 페이지간 이동, 페이지별 css제작.

---
+++ frontend/src/views/AdminMain.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-container>
+        <h2>현재 방문 고객 현황 <span>현재 고객 수 : 6명 </span></h2>
+        <div class="mx-auto" max-width="1920" style="margin-top:50px;">
+            <v-slide-group class="pa-4 my-slider" center-active dark>
+                <v-slide-item v-for="n in 15" :key="n" v-slot="{ active, toggle }">
+                    <v-card
+                        :color="active ? 'primary' : 'grey'"
+                        class="ma-4"
+                        height="600"
+                        width="400"
+                        @click="toggle"
+                    >
+                        <div class="fill-height">
+                            <div class="firstBox">
+                                <div class="col-6 customInfoBox">
+                                    <p>이미지영역</p>
+                                </div>
+                                <div
+                                    class="col-6 customInfoBox"
+                                    
+                                >
+                                    <p>준형갓</p>
+                                    <p>25세</p>
+                                    <p>남성</p>
+                                </div>
+                            </div>
+                            <div class="secondBox">
+                                <p>등급 : Diamond</p>
+                                <p>관심상품 : 없음</p>
+                                <p>
+                                    요구사항 <br />
+                                    먼저 말 걸지 말아주세요. 문의 사항이 있으면 여쭤볼게요!
+                                </p>
+                                <p>=> <v-btn @click="goCamSatisfied">만족도확인</v-btn></p>
+                            </div>
+                            <div class="thirdBox">
+                                <p>입장시간 : 2021.03.04 19:36</p>
+                                <p>퇴장시간 : 퇴장전</p>
+                            </div>
+                        </div>
+                    </v-card>
+                </v-slide-item>
+            </v-slide-group>
+        </div>
+    </v-container>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        goCamSatisfied(){
+            this.$router.push({name:'CamSatisfied'});
+        }
+    }
+};
+</script>
+<style scoped>
+@import '../assets/css/adminMain.css';
+
+</style>
---
+++ frontend/src/views/CustomerRegister.vue
@@ -0,0 +1,65 @@
+<template>
+        <v-container class="col-6 centerContent">
+            <h1 class="centerText">고객 등록</h1>
+            <div class="col-6 centerContent">
+                    <v-img
+                    lazy-src="https://picsum.photos/id/11/10/6"
+                    height="300"
+                    src="https://picsum.photos/id/11/500/300"
+                    class="profileImg col-4 centerContent"
+                    ></v-img>
+                    <v-file-input
+                    accept="image/*"
+                    label="사진 변경"
+                    dark
+                    >
+                    </v-file-input>
+                    <v-row style="margin:0; padding:0;">
+                        <v-text-field
+                        label="name"
+                        type="string"
+                        dark></v-text-field> 
+                        <v-spacer></v-spacer>
+                        <v-text-field
+                        label="나이"
+                        type="number"
+                        dark></v-text-field>                       
+                    </v-row>
+                    <v-text-field
+                    label="관심분야"
+                    type="string"
+                    dark></v-text-field>
+                    <v-text-field
+                    label="요구사항"
+                    type="string"
+                    dark></v-text-field>
+                    <v-row style="margin:0; padding:0;">
+                        <v-spacer></v-spacer>
+                            <v-btn
+                            elevation="2"
+                            class="resetBtn"
+                            >초기화</v-btn>
+
+                            <v-btn
+                            elevation="2" 
+                            class="registBtn"
+                            >등록</v-btn>
+                    </v-row>
+            </div>
+            
+
+        </v-container>
+    
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+@import '../assets/css/customerRegister.css';
+
+</style>
