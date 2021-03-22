1453ac361c4898113133a4db5109194de0f3d29c
dnjswnsthd <dnjswnsthd@naver.com>
2021-03-22 15:39:50 +0900

    Fix S04P23D102-67 : Model.py 추가된 Database에 대한 Class 작성

--- backend/app/models/model.py
+++ backend/app/models/model.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from pydantic import BaseModel
 from database import db
 
-
+# Response Member Database
 class MemberTable(db.Base):
     __tablename__ = 'member'
     uuid = Column(Integer, primary_key=True, autoincrement=True)
@@ -13,6 +13,7 @@ class MemberTable(db.Base):
     join_date = Column(String(30), nullable=False)
     image = Column(String(300), nullable=False)
 
+# Request Member Database
 class Member(BaseModel):
     uuid : int
     name: str
@@ -20,3 +21,71 @@ class Member(BaseModel):
     interests: str
     requirements: str
     image: str
+
+# Response Negative Database
+class NegativeTable(db.Base):
+    __tablename__ = 'negative'
+    uuid = Column(Integer, primary_key=True)
+    anger = Column(Integer)
+    aversion = Column(Integer)
+    sad = Column(Integer)
+    fear = Column(Integer)
+    anxiety = Column(Integer)
+
+# Request Negative Database
+class Negative(BaseModel):
+    uuid : int
+    anger: int
+    aversion: int
+    sad: int
+    fear: int
+    anxiety: int
+
+# Response Positive Database
+class PositiveTable(db.Base):
+    __tablename__ = "positive"
+    uuid = Column(Integer, primary_key=True)
+    satisfaction = Column(Integer)
+    comfortable = Column(Integer)
+    joy = Column(Integer)
+    funny = Column(Integer)
+    pride = Column(Integer)
+
+# Request Positive Database
+class Positive(BaseModel):
+    uuid: int
+    satisfaction: int
+    comfortable : int
+    joy : int
+    funny: int
+    pride: int
+    
+# Response Satisfaction Database
+class SatisfactiontTable(db.Base):
+    __tablename__ = "satisfaction"
+    uuid = Column(Integer, primary_key=True)
+    exist = Column(Boolean, nullable=False, default=False)
+
+# Request Satisfaction Database
+class Satisfaction(BaseModel):
+    uuid : int
+    exist : bool
+
+# Response Visited Database
+class VisitedTable(db.Base):
+    __tablename__ = "visited"
+    uuid = Column(Integer, primary_key=True)
+    start_visit = Column(String(20))
+    end_visit = Column(String(20))
+
+# Request Visited Database
+class Visited(BaseModel):
+    uuid : int
+    start_visit : str
+    end_visit : str
+
+
+    
+
+
+
