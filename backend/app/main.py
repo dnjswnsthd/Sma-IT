45613134572ea003ce28b65b8a79afc462df6f15
조석준 <vcxttlvcx@naver.com>
2021-03-24 14:07:09 +0900

    update S04P23D102-31 : backend reverse proxy setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from router import member_router
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 app.add_middleware(
     CORSMiddleware,
