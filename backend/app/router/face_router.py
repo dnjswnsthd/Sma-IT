82179ca9348309d4bbdc18b523d090fe1af6b7dd
dnjswnsthd <dnjswnsthd@naver.com>
2021-03-24 14:32:35 +0900

    Add S04P23D102-28 : 얼굴 인식을 통한 회원정보 조회 구현(이미지 경로 수정 필요)

--- backend/app/router/face_router.py
+++ backend/app/router/face_router.py
@@ -1,12 +1,26 @@
 from fastapi import APIRouter
 
-from utils.mask import mask_check
+from models.model import Member
+from crud import member_crud as crud
+from database.db import session
 
+import numpy as np
+
+from utils.mask import mask_check
+from utils.face import face_check
 
 router = APIRouter()
 
-
 @router.get("/{img_path:path}")
+async def mask_checking(img_path: str):
+    print(img_path)
+    members = crud.get_members(session)
+    face_compare = face_check(img_path, members)
+    return face_compare
+
+
+
+@router.get("/mask/{img_path:path}")
 async def mask_checking(img_path: str):
     print(img_path)
     mask_data = mask_check(img_path)
--- modules/face_recognition/main.py
+++ modules/face_recognition/main.py
@@ -3,13 +3,13 @@ import numpy as np
 import face_recognition
 
 # 이미지 로드
-imgElon = face_recognition.load_image_file('ImageBasic/musk1.jfif')
+imgElon = face_recognition.load_image_file('ImageBasic/mask3.jpg')
 imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
 
-imgElon_test = face_recognition.load_image_file('ImageBasic/musk2.jfif')
+imgElon_test = face_recognition.load_image_file('ImageBasic/jun.jpg')
 imgElon_test = cv2.cvtColor(imgElon_test, cv2.COLOR_BGR2RGB)
 
-imgIU = face_recognition.load_image_file('ImageBasic/IU1.jfif')
+imgIU = face_recognition.load_image_file('ImageBasic/jun2.jpg')
 imgIU = cv2.cvtColor(imgIU, cv2.COLOR_BGR2RGB)
 
 # 이미지 분석
@@ -37,8 +37,11 @@ result2 = face_recognition.compare_faces([encodeElon], encodeIU)
 print('일론 + 일론 테스트 : ', result1)
 print('일론 + IU 테스트 : ', result2)
 
+encodeElon
+print('타입 : ', type(encodeElon))
+
 # 이미지 출력
-cv2.imshow('Elon Musk', imgElon)
-cv2.imshow('Elon Musk Test', imgElon_test)
-cv2.imshow('IU', imgIU)
-cv2.waitKey(0)
+#cv2.imshow('Elon Musk', imgElon)
+#cv2.imshow('Elon Musk Test', imgElon_test)
+#cv2.imshow('IU', imgIU)
+#cv2.waitKey(0)
