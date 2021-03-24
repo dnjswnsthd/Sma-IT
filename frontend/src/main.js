7a763226db3378dc9b513d1c7f8c14d454a6c518
조준형 <cjh951114@naver.com>
2021-03-24 15:13:54 +0900

    add S04P23D102-74 : 메인페이지에서 Click버튼을 클릭 시 캡쳐한이미지를 blob으로 변환.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Axios from 'axios';
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, Axios);
 Vue.config.productionTip = false;
 
 new Vue({
