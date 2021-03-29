09b60fc33e57396b76c66b6b663209fdaea999e7
조석준 <vcxttlvcx@naver.com>
2021-03-29 16:00:08 +0900

    Add S04P23D102-38 : 결제 시 사용할 카드 등록 구현

---
+++ backend/app/crud/payment_crud.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+
+from models.payment import Payment, PaymentTable
+
+
+def select_cardInfo_by_UUID(db: Session, uuid: int):
+    return db.query(PaymentTable).filter(PaymentTable.uuid == uuid).first()
+
+def insert_cardInfo(db: Session, payment: Payment):
+    db_payment = PaymentTable()
+    db_payment.uuid = payment.uuid
+    db_payment.card_num = payment.card_num
+    db_payment.card_name = payment.card_name
+
+    db.add(db_payment)
+    db.commit()
+    db.refresh(db_payment)
+
+    return db_payment
