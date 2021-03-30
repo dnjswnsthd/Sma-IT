71d4ef21bd88962635ca7df6ac48545710acd1e2
minju75 <68409296+minju75@users.noreply.github.com>
2021-03-30 17:22:12 +0900

    Update S04P23D102-95 : Vuex를 이용해 현재 방문 중인 고객 저장 및 확인

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // axios 객체 생성
 export default axios.create({
     
-    baseURL: 'https://j4d102.p.ssafy.io', // baseUrl
+    baseURL: 'https://j4d102.p.ssafy.io/api', // baseUrl
     headers: {
         'Content-type': 'application/json',
     },
