88419b7f7d0ad433d330a4c92a68b73a1be1f2a9
김두상 <ds021346@gmail.com>
2021-03-31 17:16:06 +0900

    Add S04P23D102-28 : visited 테이블 연결 및 처리

--- backend/app/router/face_router.py
+++ backend/app/router/face_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, File, UploadFile
 
-from models.member import Member
-from crud import member_crud as crud
+from models.visited import Visited
+from crud import member_crud, visited_crud
+
 from database.db import session
 
 import numpy as np
@@ -15,27 +16,42 @@ from utils.face import face_check
 router = APIRouter()
 
 
-@router.post("/")
-async def mask_checking(file: UploadFile = File(...)):
-    members = crud.get_members(session)
+@router.post("/{start_visted}" , summary="얼굴 인식 및 마스크 인식")
+async def face_mask_checking(time: str, file: UploadFile = File(...)):
+    # 회원정보 전체 조회
+    members = member_crud.get_members(session)
+    
+    # 이미지 저장
     content = await file.read()
     with open(f'../img/cam_img/{file.filename}', 'wb') as fh:
         fh.write(content)
-    face_data = face_check(file.filename, members)
-    # mask_data = mask_check()
+   
+    #얼굴인식 및 마스크 인식
+    face_data = face_mask_check(file.filename, members)
+
+    # 입장 시간 저장
+    visited_crud.create_visited(session, face_data["member"].uuid, start_visted)
+    
     return face_data
 
-@router.get("/test/{img_path:path}")
-async def mask_checking(img_path: str):
-    print(img_path)
-    members = crud.get_members(session)
-    face_compare = face_check(img_path, members)
-    return face_compare
+@router.post("/fase", summary="얼굴인식")
+async def face_checking(file: UploadFile = File(...)):
+    # 회원정보 전체 조회
+    members = member_crud.get_members(session)
+    
+    # 이미지 저장
+    content = await file.read()
+    with open(f'../img/cam_img/{file.filename}', 'wb') as fh:
+        fh.write(content)
+   
+    #얼굴인식
+    face_data = face_check(file.filename, members)
 
+    return face_data
 
-
-@router.get("/mask/{img_path:path}")
+@router.get("/mask/{img_path:path}", summary="마스크 인식")
 async def mask_checking(img_path: str):
     print(img_path)
     mask_data = mask_check(img_path)
     return mask_data
+
--- backend/app/utils/face.py
+++ backend/app/utils/face.py
@@ -6,7 +6,7 @@ from .mask import mask_check
 
 
 
-def face_check(img_path: str, members: Member):
+def face_mask_check(img_path: str, members: Member):
     print("이까지왔니?")
     # 비교할 이미지 로드
     img_path = "../img/cam_img/" + img_path 
@@ -40,4 +40,36 @@ def face_check(img_path: str, members: Member):
 
     return "등록된 회원이 아닙니다"
    
- 
+def face_check(img_path: str, members: Member):
+    print("이까지왔니?")
+    # 비교할 이미지 로드
+    img_path = "../img/cam_img/" + img_path 
+    img = face_recognition.load_image_file(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    print(img_path)
+    if len(face_recognition.face_locations(img)) == 0:
+      print("얼굴인식 실패")
+      return "얼굴인식 실패"
+
+    user = face_recognition.face_encodings(img)[0]
+
+    for member in members:
+        # 디비에 데이터를 가져와야함(임시 데이터 준형이 마스크얼굴)
+        memberimg_path = "../img/member_img/" + member.image
+        memberimg = face_recognition.load_image_file(memberimg_path)
+        memberimg = cv2.cvtColor(memberimg, cv2.COLOR_BGR2RGB)
+
+        #기존 이용자 얼굴 분석
+        memberface = face_recognition.face_encodings(memberimg)[0]
+
+        # 얼굴 비교 
+        result = face_recognition.compare_faces([memberface], user)
+
+        # 얼굴이 같으면 바로 리턴 하고 맴버정보 제공
+        if result[0]:
+            result = dict(member = member)
+            return result
+    #print("등록된 회원이 아닙니다")
+
+    return "등록된 회원이 아닙니다"
