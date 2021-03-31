af51594dcc7df50365beac8bf9d6b4ff3145cc94
조준형 <cjh951114@naver.com>
2021-03-31 16:21:42 +0900

    update S04P23D102-98, S04P23D102-99  : 관리자페이지와 만족도페이지에 차트그리기

--- frontend/src/views/AdminMain.vue
+++ frontend/src/views/AdminMain.vue
@@ -30,16 +30,39 @@
                             </div>
 
                             <div class="secondBox">
-                                <p>등급 : Diamond</p>
-                                <p>관심상품 : 없음</p>
+                                <p>관심상품 : {{ customer.interest }}</p>
                                 <p>
                                     요구사항 <br />
-                                    먼저 말 걸지 말아주세요.<br />
-                                    문의 사항이 있으면 여쭤볼게요!
+                                    {{ customer.requirements }}
                                 </p>
-                                <button class="satisfiedBtn" @click="goCamSatisfied">
-                                    => 만족도 확인
-                                </button>
+
+                                <v-dialog v-model="satisfyDialog" width="500">
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <button
+                                            class="satisfiedBtn"
+                                            v-bind="attrs"
+                                            v-on="on"
+                                            @click="goCamSatisfied(customer.uuid)"
+                                        >
+                                            => 만족도 확인
+                                        </button>
+                                    </template>
+
+                                    <v-card>
+                                        <chart></chart>
+                                        <v-divider></v-divider>
+                                        <v-card-actions>
+                                            <v-spacer></v-spacer>
+                                            <v-btn
+                                                color="primary"
+                                                text
+                                                @click="satisfyDialog = false"
+                                            >
+                                                닫기
+                                            </v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-dialog>
                             </div>
                             <div class="thirdBox">
                                 <p>입장시간 : sadfdf</p>
@@ -54,19 +77,41 @@
 </template>
 <script>
 import { mapGetters } from 'vuex';
-
+import Chart from '../components/Chart.vue';
+import http from '../api/axios';
 export default {
+    components: {
+        chart: Chart,
+    },
     data() {
         return {
             customers: [],
+            satisfyDialog: false,
+            labels: [
+                'anger',
+                'contempt',
+                'disgust',
+                'fear',
+                'happiness',
+                'neutral',
+                'sadness',
+                'surprise',
+            ],
+            data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
         };
     },
     computed: {
         ...mapGetters(['getCustomerInfo']),
     },
     methods: {
-        goCamSatisfied() {
-            this.$router.push({ name: 'CamSatisfied' });
+        goCamSatisfied(uuid) {
+            http.get(`/member/emotion/${uuid}`)
+                .then(() => {
+                    alert('구왁');
+                })
+                .catch(() => {
+                    alert('안데스산맥');
+                });
         },
     },
 };
