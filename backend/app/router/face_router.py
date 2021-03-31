c1d40d9e9c5df8c383a0414951d03639e0763053
dnjswnsthd <dnjswnsthd@naver.com>
2021-03-31 20:20:12 +0900

    Add S04P23D102-28 : 얼굴인식, 마스크인식 분리, 감정정보 등록 및 visted CRUD

--- backend/app/router/face_router.py
+++ backend/app/router/face_router.py
@@ -12,11 +12,11 @@ import cv2
 
 from utils.mask import mask_check
 from utils.face import face_check
-
+from utils.face import face_mask_check
 router = APIRouter()
 
 
-@router.post("/{start_visted}" , summary="얼굴 인식 및 마스크 인식")
+@router.post("/mask/{start_visted}" , summary="얼굴 인식 및 마스크 인식")
 async def face_mask_checking(start_visted: str, file: UploadFile = File(...)):
     # 회원정보 전체 조회
     members = member_crud.get_members(session)
@@ -28,10 +28,10 @@ async def face_mask_checking(start_visted: str, file: UploadFile = File(...)):
    
     #얼굴인식 및 마스크 인식
     face_data = face_mask_check(file.filename, members)
-
+    print("전 ",face_data["member"].uuid)
     # 입장 시간 저장
     visited_crud.create_visited(session, face_data["member"].uuid, start_visted)
-    
+    print("후 ",face_data["member"].uuid)
     return face_data
 
 @router.post("/onlyface", summary="얼굴인식")
@@ -49,7 +49,7 @@ async def face_checking(file: UploadFile = File(...)):
 
     return face_data
 
-@router.get("/mask/{img_path:path}", summary="마스크 인식")
+@router.get("/onlymask/{img_path:path}", summary="마스크 인식")
 async def mask_checking(img_path: str):
     print(img_path)
     mask_data = mask_check(img_path)
