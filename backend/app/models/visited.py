cea7c96df0d1e10695ea8bead88c833cd5cacbd2
조석준 <vcxttlvcx@naver.com>
2021-04-06 22:47:48 +0900

    Fix S04P23D102-21 : 고객 정보에서 입장 시간과 퇴장 시간 포함하여 제공

--- backend/app/models/visited.py
+++ backend/app/models/visited.py
@@ -6,18 +6,14 @@ from database import db
 # Response Visited Database
 class VisitedTable(db.Base):
     __tablename__ = "visited"
-    visited_id = Column(Integer, primary_key=True, autoincrement=True)  
+    visited_id = Column(Integer, primary_key=True, autoincrement=True)
     start_visit = Column(String(20))
     end_visit = Column(String(20))
     uuid = Column(Integer)
 
+
 # Request Visited Database
 class Visited(BaseModel):
-    uuid : int
+    uuid: int
     start_visit: str
-
-
-    
-
-
-
+    end_visit: str
