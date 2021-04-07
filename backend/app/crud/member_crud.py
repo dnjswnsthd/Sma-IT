bfda0e73b77835b6ac9d60b5118b0696dd2d8ed3
조석준 <vcxttlvcx@naver.com>
2021-04-07 15:39:26 +0900

    Update S04P23D102-124 : 모든 파일에 주석 추가

--- backend/app/crud/member_crud.py
+++ backend/app/crud/member_crud.py
@@ -9,14 +9,17 @@ from models.visited import VisitedTable
 import os
 
 
+# skip ~ limit 까지의 고객 정보 및 방문 시간, 퇴장 시간 가져오기
 def get_members(db: Session, skip: int = 0, limit: int = 100):
+    # 가장 최근 방문 시간을 가져오기 위한 Subquery 생성
     stmt = db.query(VisitedTable.uuid, func.max(VisitedTable.start_visit).label(
         'start_visit'), func.max(VisitedTable.end_visit).label('end_visit')).group_by(VisitedTable.uuid).subquery()
-    # temp, start_visit, end_visit = db.query(MemberTable, stmt.c.start_visit, stmt.c.end_visit.label('end_visit')).join(
-    #     stmt, MemberTable.uuid == stmt.c.uuid).order_by(MemberTable.uuid).offset(skip).limit(limit).all()
+
     members = []
+    # member 테이블과 subquery의 join을 통한 고객 정보와 방문 시간, 퇴장 시간 가져오기
     for member, start_visit, end_visit in db.query(MemberTable, stmt.c.start_visit, stmt.c.end_visit).join(
             stmt, MemberTable.uuid == stmt.c.uuid).order_by(MemberTable.uuid).offset(skip).limit(limit):
+        # 결과를 json형식으로 제공하기 위하여 dict 형으로 만들어 줌
         mem = {
             'uuid': member.uuid,
             'name': member.name,
@@ -30,23 +33,27 @@ def get_members(db: Session, skip: int = 0, limit: int = 100):
         }
         members.append(mem)
 
-    # return db.query(MemberTable).order_by(MemberTable.uuid).offset(skip).limit(limit).all()
     return members
 
 
+# 고객 이름을 기반으로 고객 정보 가져오기
 def get_member_by_name(db: Session, member_name: str):
     return db.query(MemberTable).filter(MemberTable.name == member_name).first()
 
 
+# uuid를 기반으로 고객 정보 가져오기
 def get_member_by_uuid(db: Session, member_uuid: int):
     return db.query(MemberTable).filter(MemberTable.uuid == member_uuid).first()
 
 
+# 등록된 사진 이름으로 고객 정보 가져오기
 def get_member_by_image(db: Session, member_img: int):
     return db.query(MemberTable).filter(MemberTable.image == member_img).first()
 
 
+# 고객 정보 등록
 def create_member(db: Session, member: Member, time: str):
+    # 등록을 위한 고객 정보 기록
     db_member = MemberTable()
     db_member.name = member.name
     db_member.age = member.age
@@ -56,16 +63,19 @@ def create_member(db: Session, member: Member, time: str):
     db_member.image = member.image
 
     try:
+        # 고객 정보 등록
         db.add(db_member)
         db.commit()
         db.refresh(db_member)
     except:
+        # 문제 발생 시 rollback
         db.rollback()
         raise SQLAlchemyError
 
     return db_member
 
 
+# 고객이 등록한 이미지 이름을 DB에 저장
 def update_image_member(db: Session, db_member: MemberTable, image: int):
     try:
         db_member.image = str(image) + ".jpg"
@@ -77,6 +87,7 @@ def update_image_member(db: Session, db_member: MemberTable, image: int):
     return db_member
 
 
+# uuid 기반으로 고객 정보 삭제
 def delete_member_by_uuid(db: Session, member_uuid: int):
     try:
         db_member = db.query(MemberTable).filter(
@@ -92,6 +103,7 @@ def delete_member_by_uuid(db: Session, member_uuid: int):
     return db_member
 
 
+# 고객 정보 수정
 def update_member(db: Session, db_member: MemberTable, member: UpdateMember):
     try:
         db_member.uuid = member.uuid
--- backend/app/crud/payment_crud.py
+++ backend/app/crud/payment_crud.py
@@ -4,10 +4,12 @@ from sqlalchemy.exc import SQLAlchemyError
 from models.payment import Payment, PaymentTable
 
 
+# 고객의 uuid를 기반으로 등록 된 카드 정보 제공
 def select_cardInfo_by_UUID(db: Session, uuid: int):
     return db.query(PaymentTable).filter(PaymentTable.uuid == uuid).first()
 
 
+# 고객의 카드 정보 등록
 def insert_cardInfo(db: Session, payment: Payment):
     db_payment = PaymentTable()
     db_payment.uuid = payment.uuid
